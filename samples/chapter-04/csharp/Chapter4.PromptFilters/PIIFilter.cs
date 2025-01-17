9d3d516dab439cf9c63e2a15dd679869d0105dbc
Willem Meints <1550763+wmeints@users.noreply.github.com>
2025-01-17 06:49:09 +0100

    Add example for prompt filters

--- samples/chapter-04/csharp/Chapter4.PromptFilters/PIIFilter.cs
+++ samples/chapter-04/csharp/Chapter4.PromptFilters/PIIFilter.cs
@@ -1,6 +1,35 @@
-﻿namespace Chapter4.PromptFilters;
+﻿using Microsoft.SemanticKernel;
 
-public class PIIFilter
+namespace Chapter4.PromptFilters;
+
+public class PIIFilter: IFunctionInvocationFilter, IPromptRenderFilter
 {
-    
+    public async Task OnFunctionInvocationAsync(FunctionInvocationContext context, Func<FunctionInvocationContext, Task> next)
+    {
+        // This function is called when the prompt is executed using the LLM provider.
+        // We can't modify the prompt at this point. We can only filter the outpput.
+        
+        await next(context);
+        
+        var output = context.Result.GetValue<string>();
+        
+        //TODO: Replace PII in the output
+
+        // Create a new function result, based on the old one, but with the filtered value.
+        context.Result = new FunctionResult(context.Result, output);
+    }
+
+    public async Task OnPromptRenderAsync(PromptRenderContext context, Func<PromptRenderContext, Task> next)
+    {
+        // This function is called when the prompt is rendered. This is where
+        // we can filter the contents of the prompt before it's submitted.
+        
+        var renderedPrompt = context.RenderedPrompt;
+        
+        //TODO: Filter the prompt contents
+        
+        // Replace the original prompt with the filtered prompt.
+        context.RenderedPrompt = renderedPrompt; 
+        await next(context);
+    }
 }
