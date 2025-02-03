4921efc3d79ce8dadc99c71c01ba3405e358ab48
Willem Meints <1550763+wmeints@users.noreply.github.com>
2025-02-03 16:08:56 +0100

    Improve sample code

---
+++ samples/chapter-06/csharp/Chapter6.FunctionFilters/LoggingFunctionFilter.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.SemanticKernel;
+
+namespace Chapter6.FunctionFilters;
+
+public class LoggingFunctionFilter : IFunctionInvocationFilter
+{
+    public async Task OnFunctionInvocationAsync(FunctionInvocationContext context, Func<FunctionInvocationContext, Task> next)
+    {
+        Console.WriteLine($"Invoking {context.Function.Name}");
+
+        foreach (var param in context.Arguments)
+        {
+            Console.WriteLine($"Parameter '{param.Key}' has value '{param.Value}");
+        }
+        
+        await next(context);
+        
+        Console.WriteLine($"Done invoking {context.Function.Name}");
+    }
+}
---
+++ samples/chapter-06/csharp/Chapter6.FunctionFilters/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Security.Cryptography;
+using Chapter6.FunctionFilters;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.AzureOpenAI;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using Microsoft.SemanticKernel.PromptTemplates.Handlebars;
+
+var configuration = new ConfigurationBuilder()
+    .AddUserSecrets<Program>()
+    .Build();
+
+var kernelBuilder = Kernel.CreateBuilder()
+    .AddAzureOpenAIChatCompletion(
+        deploymentName: configuration["LanguageModel:DeploymentName"]!,
+        endpoint: configuration["LanguageModel:Endpoint"]!,
+        apiKey: configuration["LanguageModel:ApiKey"]!
+    );
+
+kernelBuilder.Plugins.AddFromObject(new TimePlugin());
+kernelBuilder.Services.AddTransient<IFunctionInvocationFilter, LoggingFunctionFilter>();
+
+var arguments = new KernelArguments(new AzureOpenAIPromptExecutionSettings
+{
+    FunctionChoiceBehavior = FunctionChoiceBehavior.Auto()
+});
+
+var kernel = kernelBuilder.Build();
+var response = await kernel.InvokePromptAsync("Can you tell me what time it is?", arguments);
+
+Console.WriteLine(response.ToString());
