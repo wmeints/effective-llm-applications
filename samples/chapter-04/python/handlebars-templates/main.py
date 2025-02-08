eb475b92ea0bf8f920aa8c522c95c2f4ced1c366
Willem Meints <1550763+wmeints@users.noreply.github.com>
2025-02-08 14:39:11 +0100

    Add sample to demonstrate handlebars templates

---
+++ samples/chapter-04/python/handlebars-templates/main.py
@@ -0,0 +1,35 @@
+import asyncio
+from pathlib import Path
+
+from dotenv import load_dotenv
+from semantic_kernel import Kernel
+from semantic_kernel.connectors.ai.open_ai import AzureChatCompletion
+from semantic_kernel.kernel import KernelArguments
+from settings import ApplicationSettings
+
+load_dotenv(".venv")
+
+app_settings = ApplicationSettings()
+kernel = Kernel()
+
+kernel.add_service(
+    AzureChatCompletion(
+        api_key=app_settings.api_key,
+        deployment_name=app_settings.deployment_name,
+        endpoint=app_settings.endpoint,
+    )
+)
+
+
+async def main():
+    args = KernelArguments(dish="pizza", ingredients=["mozzarella", "spinach"])
+    prompt_template = (Path(__file__).parent / "prompt.txt").read_text()
+
+    response = await kernel.invoke_prompt(
+        prompt_template, arguments=args, template_format="handlebars"
+    )
+
+    print(response)
+
+
+asyncio.run(main())
