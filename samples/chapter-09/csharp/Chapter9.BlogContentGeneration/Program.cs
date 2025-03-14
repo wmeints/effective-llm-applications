31a2c555eeb6d6c13ed2e21e5b291e268d8daf36
Willem Meints <1550763+wmeints@users.noreply.github.com>
2025-03-14 08:02:14 +0100

    Add skeleton for first example in chapter 9

---
+++ samples/chapter-09/csharp/Chapter9.BlogContentGeneration/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Plugins.Web;
+using Microsoft.SemanticKernel.Plugins.Web.Google;
+
+var configuration = new ConfigurationBuilder()
+    .AddUserSecrets<Program>()
+    .Build();
+
+var kernel = Kernel.CreateBuilder()
+    .AddAzureOpenAIChatCompletion(
+        configuration["LanguageModel:DeploymentName"]!,
+        configuration["LanguageModel:Endpoint"]!,
+        configuration["LanguageModel:ApiKey"]!
+    ).Build();
+
+kernel.Plugins.AddFromObject(new WebSearchEnginePlugin(new GoogleConnector(
+    apiKey: configuration["Google:ApiKey"]!,
+    searchEngineId: configuration["Google:SearchEngineId"]!)));
