0d426d91c9da21f6d4dd788a7aff79c5d119d935
Willem Meints <1550763+wmeints@users.noreply.github.com>
2025-07-18 11:01:57 +0200

    Add sample to demonstrate Azure agents

---
+++ samples/chapter-11/csharp/Chapter11.StatefulAgent/Program.cs
@@ -0,0 +1,26 @@
+﻿using Azure.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Agents.AzureAI;
+using Microsoft.SemanticKernel.ChatCompletion;
+
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile(Path.Join("appsettings.json"), optional: true, reloadOnChange: true)
+    .AddUserSecrets<Program>()
+    .Build();
+
+var persistentAgentsClient = AzureAIAgent.CreateAgentsClient(
+    configuration["Foundry:Endpoint"]!,
+    new DefaultAzureCredential());
+
+var persistentAgent = persistentAgentsClient.Administration.GetAgent(
+    configuration["Foundry:AgentId"]);
+
+var agent = new AzureAIAgent(persistentAgent, persistentAgentsClient);
+
+var agentThread = new AzureAIAgentThread(persistentAgentsClient);
+var responseStream = agent.InvokeAsync(new ChatMessageContent(AuthorRole.User, "..."), agentThread);
+
+
+
